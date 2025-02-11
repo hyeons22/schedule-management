0697adfb21c62832af0d708f4e5f7b2e403b5778
hyeon <ski0123@naver.com>
2025-02-11 20:39:08 +0900

    lv1 Schedule 생성

--- src/main/java/com/example/schedulemanagement/entity/Schedule.java
+++ src/main/java/com/example/schedulemanagement/entity/Schedule.java
@@ -1,18 +1,13 @@
 package com.example.schedulemanagement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor
-public class Schedule {
+public class Schedule extends DateEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,14 +16,10 @@ public class Schedule {
     private String name;
     private String title;
     private String task;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
 
-    public Schedule(String name, String title, String task, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public Schedule(String name, String title, String task) {
         this.name = name;
         this.title = title;
         this.task = task;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
     }
 }
