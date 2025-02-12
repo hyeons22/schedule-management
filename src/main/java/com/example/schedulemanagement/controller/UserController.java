0c9baea4a00b8d9620e6146109fb88bc7e300432
hyeon <ski0123@naver.com>
2025-02-12 11:50:31 +0900

    feat : 유저생성 기능추가

--- src/main/java/com/example/schedulemanagement/controller/UserController.java
+++ src/main/java/com/example/schedulemanagement/controller/UserController.java
@@ -1,10 +1,24 @@
 package com.example.schedulemanagement.controller;
 
+import com.example.schedulemanagement.dto.UserRequestDto;
+import com.example.schedulemanagement.dto.UserResponseDto;
+import com.example.schedulemanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    // 유저 생성
+    @PostMapping("/users")
+    public ResponseEntity<UserResponseDto> save(@RequestBody UserRequestDto requestDto){
+        return ResponseEntity.ok(userService.save(requestDto));
+    }
 
 }
--- src/main/java/com/example/schedulemanagement/service/UserService.java
+++ src/main/java/com/example/schedulemanagement/service/UserService.java
@@ -1,9 +1,25 @@
 package com.example.schedulemanagement.service;
 
+import com.example.schedulemanagement.dto.UserRequestDto;
+import com.example.schedulemanagement.dto.UserResponseDto;
+import com.example.schedulemanagement.entity.User;
+import com.example.schedulemanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    // 유저 생성
+    @Transactional
+    public UserResponseDto save(UserRequestDto requestDto) {
+        User user = new User(requestDto.getName(), requestDto.getEmail());
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDto(savedUser.getId(), savedUser.getName(), savedUser.getEmail());
+    }
 }
