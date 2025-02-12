f5ead8e2be0c7e86f5ef233f7f97dd1933796859
hyeon <ski0123@naver.com>
2025-02-12 11:01:12 +0900

    lv2 entity 에 User 생성

---
+++ src/main/java/com/example/schedulemanagement/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.schedulemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+public class User extends DateEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
